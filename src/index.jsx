b595c44b195ea3b386399fd674accbd9f8a0c9fd
mindorysidory <107872961+mindorysidory@users.noreply.github.com>
2025-07-21 23:04:18 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'  // ← .jsx 확장자 제거
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
