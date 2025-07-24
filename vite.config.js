f471689559064bc004f06a21268528cadb04a809
mindorysidory <107872961+mindorysidory@users.noreply.github.com>
2025-07-24 11:10:07 +0900

    배포 수정 again

--- vite.config.js
+++ vite.config.js
@@ -7,14 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true
-      },
-      includeAssets: [
-        'favicon.ico', 
-        'icons/*.png',
-        'manifest.json'
-      ],
+      includeAssets: ['favicon.ico'],
       manifest: {
         name: '쿠폰랩 - 최고의 할인 쿠폰 모음',
         short_name: '쿠폰랩',
@@ -24,72 +17,22 @@ export default defineConfig({
         display: 'standalone',
         scope: '/',
         start_url: '/',
-        orientation: 'portrait',
         icons: [
-          {
-            src: 'icons/icon-32x32.png',
-            sizes: '32x32',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-96x96.png',
-            sizes: '96x96',
-            type: 'image/png'
-          },
-          {
-            src: 'icons/icon-180x180.png',
-            sizes: '180x180',
-            type: 'image/png',
-            purpose: 'apple-touch-icon'
-          },
           {
             src: 'icons/icon-192x192.png',
             sizes: '192x192',
-            type: 'image/png',
-            purpose: 'any maskable'
+            type: 'image/png'
           },
           {
             src: 'icons/icon-512x512.png',
             sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable'
+            type: 'image/png'
           }
         ]
       },
       workbox: {
-        globPatterns: ['**/*.{js,css,html}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/fonts\.googleapis\.com/,
-            handler: 'StaleWhileRevalidate',
-            options: {
-              cacheName: 'google-fonts-stylesheets',
-            },
-          },
-          {
-            urlPattern: /^https:\/\/fonts\.gstatic\.com/,
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'google-fonts-webfonts',
-              expiration: {
-                maxEntries: 30,
-                maxAgeSeconds: 60 * 60 * 24 * 365, // 1년
-              },
-            },
-          }
-        ]
+        globPatterns: ['**/*.{js,css,html}']
       }
     })
-  ],
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
-  },
-  publicDir: 'public'
+  ]
 })
