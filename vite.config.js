f460fd905b71009edd549ce1b6b1b18e0fdde543
mindorysidory <107872961+mindorysidory@users.noreply.github.com>
2025-07-24 11:21:45 +0900

    error control

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,18 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: './index.html'
+        main: 'index.html'
       }
     }
   },
-  publicDir: 'public'
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  }
 })
